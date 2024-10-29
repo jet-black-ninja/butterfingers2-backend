8cc9293cde6919fad52a802f43245ee250c30b59
jet_black_ninja <pramodkmr958@gmail.com>
2024-10-29 16:21:31 +0530

    added details changing functionality

--- routes/auth.router.ts
+++ routes/auth.router.ts
@@ -3,6 +3,10 @@ import User, { UserProps } from "../models/User.model";
 import Profile from "../models/Profile.model";
 import jwt from "jsonwebtoken";
 import ValidationError from "../errors/ValidationError";
+import { AuthenticatedRequest } from "../types";
+import NotFoundError from "../errors/NotFoundError";
+import OauthUser from "../models/OauthUser.model";
+import { isValidObjectId } from "mongoose";
 
 const frontendUrl =
   process.env.NODE_ENV === "developments" || !process.env.FRONTEND_URL
@@ -57,6 +61,83 @@ export async function Login(
       sameSite: "strict",
     });
     res.json({ message: "Logged in SuccessFully" });
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function Logout(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+    try {
+        res.clearCookie('token', { secure: true, httpOnly: true, sameSite: "strict" });
+        res.json({
+            message: 'Logged Out successfully',
+        })
+    } catch (err) {
+        next(err);
+    }
+}
+
+export async function ChangeUsername(
+  req: AuthenticatedRequest<any, Response, { password: string; newUsername: string }>,
+  res: Response,
+  next: NextFunction
+) {
+  const { password, newUsername } = req.body;
+  const username = req.user!.username;
+  try {
+    if (!req.user?.platform) {
+      const user = (await User.findOne({ username }));
+      const passwordMatch = await user?.comparePassword(password);
+
+      if (!user) {
+        throw new NotFoundError('User Not Found')
+      }
+      if (!passwordMatch) {
+        throw new ValidationError("Incorrect Password", "password");
+      }
+      user.$set('username', newUsername);
+      await user.save();
+    } else {
+      const oauthUser = await OauthUser.findOne({ username });
+      if (!oauthUser) {
+        throw new NotFoundError('User Not Found');
+      }
+      oauthUser.$set('username', newUsername);
+      await oauthUser.save();
+    }
+    res.json({username: newUsername})
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function ChangePassword(
+  req: AuthenticatedRequest<any, Response, { oldPassword: string; newPassword: string }>,
+  res: Response,
+  next: NextFunction
+) {
+  if (req.user?.platform) {
+    next(new Error());
+  }
+
+  const { oldPassword, newPassword } = req.body;
+  const username = req.user!.username;
+  try {
+    const user = (await User.findOne({ username }));
+    const passwordMatch = await user?.comparePassword(oldPassword);
+    if (!user) {
+      throw new NotFoundError('User Not Found')
+    }
+    if (!passwordMatch) {
+      throw new ValidationError('Passwords do not match', 'Password');
+    }
+    user.password = newPassword;
+    user.save();
+    res.json({ message: 'Password changed successfully' });
   } catch (err) {
     next(err);
   }
